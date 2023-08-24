256cf66964a4aff08f4e7a712f1a7ace29014fc6
DoubleA-N <aaeyysm@gmail.com>
2023-08-25 00:40:00 +0700

    fix(icon): fix the space

--- src/app/components/common/icon/index.js
+++ src/app/components/common/icon/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import React from "react"
 
-const Icon = ({ alt, className,height, src, width }) => (
+const Icon = ({ alt, className, height, src, width }) => (
   <span className={className} role="presentation">
     <Image alt={alt} height={height || 15} src={src} width={width || 15} />
   </span>
